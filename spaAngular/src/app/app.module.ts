2dae2022b6f8b0cba54cf61e53e7f7b1177e4ba0
lnpq.dev <lnpq.dev@gmail.com>
2020-04-21 19:36:53 -0300

    SPA com 2 componentes, sendo uma página inicial e uma página de contato com formulário

---
+++ spaAngular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
